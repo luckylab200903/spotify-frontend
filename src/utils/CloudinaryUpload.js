91e9c325b61ce103f76ddfd0d77676a183ff751d
keshav kumar singh <kumarkeshavsingh200903@gmail.com>
2024-04-14 20:03:18 +0530

    added files

--- src/utils/CloudinaryUpload.js
+++ src/utils/CloudinaryUpload.js
@@ -5,8 +5,8 @@ const CloudinaryUpload = ({ setUrl, setSongName }) => {
   const uploadSongWidget = () => {
     let myUploadWidget = openUploadWidget(
       {
-        cloudName: process.env.CLOUD_NAME,
-        uploadPreset: process.env.UPLOAD_PRESET,
+        cloudName: "dtekkvnmz", // Replace with your Cloudinary cloud name
+        uploadPreset: "spotify-clone", // Replace with your Cloudinary upload preset
         sources: ["local"],
       },
       function (error, result) {
